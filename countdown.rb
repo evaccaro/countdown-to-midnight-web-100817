63f296dca19046abdbe692b6629483bbcd7a7b73
Elisa Vaccaro <erv230@nyu.edu>
2017-09-26 18:03:18 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,8 @@
 #write your code here
-
 def countdown(number)
   counter = number
   while counter > 0
-    puts "#{number} SECOND(S)!"
+    puts "#{counter} SECOND(S)!"
     counter -= 1
   end
   "HAPPY NEW YEAR!"
